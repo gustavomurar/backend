70792bfd88c7a89debb67de1a99984c418b6f768
gustavomurar <gustavomurarosoares3@gmail.com>
2025-02-04 15:09:30 -0300

    parte 2

--- alunos/models.py
+++ alunos/models.py
@@ -6,3 +6,10 @@ class Estado(models.Model):
 
     def __str__(self):
         return f"{self.nome} ({self.sigla})"
+    
+class Cidade(models.Model):
+    nome = models.CharField(max_length=50)
+    estadp = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
+    def _str_(self):
+        return self.nome
