cd7cf2369aff0c7b5dcce5cbfd42488bade7a212
gustavomurar <gustavomurarosoares3@gmail.com>
2025-02-11 15:18:18 -0300

    parte 2

--- alunos/serializers.py
+++ alunos/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer , SerializerMethodField 
 from .models import Estado , Cidade , Pessoa
 
 
